176261962934801d81de15a49577f71b232a4cd8
Juan8516 <juan.ind85@gmail.com>
2024-03-30 11:20:43 -0500

    Create TestCreateScript.cs

---
+++ ProgrammingUnity/Assets/Scripts/TestCreateScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProVideoGames
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+    
+    }
+}
