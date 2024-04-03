3d9b5b9a5424e6c6c29798c05a7bbf9c29537a9b
Juan8516 <juan.ind85@gmail.com>
2024-04-03 14:12:35 -0500

    Scripts Folder
    Se crean scripts variables.

---
+++ ProgrammingUnity/Assets/Scripts/Variables.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace ProVideoGames.FunfamentosProgramacion
+{
+    public class Variables : MonoBehaviour
+    {
+        //Partes de una variable
+        // - Modificador de acceso
+        // Public 
+        // Private
+
+        // - Tipos de datos
+        // int 
+        // float
+        // bool
+        // string
+
+        // - Nombre
+        // Nombre acorde a lo que se esta usando y el tipo de dato: age, isDead etc
+
+        // - Valor por defecto (opcional)
+
+
+        // - Ejemplos
+        public string playerName;
+    }
+}
