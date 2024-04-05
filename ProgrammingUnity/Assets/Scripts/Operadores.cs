cca388c3454124c5ce2ee9663dbbda5992a6b455
Juan8516 <juan.ind85@gmail.com>
2024-04-05 13:25:28 -0500

    Update Operadores.cs
    Ejemplos de los operadores.

--- ProgrammingUnity/Assets/Scripts/Operadores.cs
+++ ProgrammingUnity/Assets/Scripts/Operadores.cs
@@ -2,10 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ProVideoGames.FunfamentosProgramacion
+namespace ProVideoGames.FundamentosProgramacion
 {
     public class Operadores : MonoBehaviour
     {
-    
+        int myInt;
+        bool myBool;
+
+        private void Start()
+        {
+            //Operadores aritmeticos
+            myInt = 2 + 2;
+            myInt = 4 - 2;
+            myInt = 6 * 9;
+            myInt = 8 / 2;
+
+            //Operadores asignacion
+            myInt += 2;
+            myInt -= 2;
+            myInt *= 2;
+            myInt /= 2;
+
+            //Operadores relacionales
+            myBool = 2 > 1;
+            myBool = 2 < 1;
+            myBool = 2 == 1;
+            myBool = 2 >= 1;
+            myBool = 2 <= 1;
+            myBool = 2 != 1;
+
+            //Operadores Logicos
+
+
+        }
     }
 }
