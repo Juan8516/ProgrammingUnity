9a421f5db243ebadac5082c3b3e49133165ae189
Juan8516 <juan.ind85@gmail.com>
2024-04-05 13:15:39 -0500

    Creacion Script Operadores
    Se crea script de operadores.

---
+++ ProgrammingUnity/Assets/Scripts/Operadores.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProVideoGames.FunfamentosProgramacion
+{
+    public class Operadores : MonoBehaviour
+    {
+    
+    }
+}
