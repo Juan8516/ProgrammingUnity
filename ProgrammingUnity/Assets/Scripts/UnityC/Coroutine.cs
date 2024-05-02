229c1af52cf054002d5fc7735ba3f31df33467d1
Juan Manuel Salazar <134428143+Juan8516@users.noreply.github.com>
2024-05-02 11:45:29 -0500

    Script Coroutines
    Se crea Script Coroutunes Star, Stop, Params

---
+++ ProgrammingUnity/Assets/Scripts/UnityC/Coroutine.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProVideoGames.FundamentosProgramacion
+{
+    public class Coroutine : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            #region Start
+            //Initialization
+            StartCoroutine(CallCoroutine());
+
+            StartCoroutine("CallCoroutine");
+
+            //Coroutine myCoroutine;
+            //myCoroutine = StartCoroutineCallCoroutine();
+            #endregion
+
+            #region stop
+            //Stop
+            StopCoroutine(CallCoroutine());
+
+            StopCoroutine("CallCoroutine");
+
+            StopCoroutine("Nombre Coroutine");
+
+            StopAllCoroutines();
+            #endregion
+
+            #region Parametros
+            //Parametros
+            StartCoroutine(CallCoroutineWithParams(7.3f, 16, "Test"));
+
+            Coroutine myCoroutine;
+            //myCoroutine = StartCoroutine(CallCoroutineWithParams(7.3f, 16, "Test"));
+
+            #endregion
+
+
+        }
+
+        private IEnumerator CallCoroutine()
+        {
+            yield return null;
+        }
+
+        private IEnumerator CallCoroutineWithParams(float myFloat, int myInt, string myString)
+        {
+            yield return null;
+        }
+    }
+}
