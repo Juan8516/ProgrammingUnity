07495b62f77360808345264a1cf4038cccc22967
Juan Manuel Salazar <134428143+Juan8516@users.noreply.github.com>
2024-05-10 14:52:32 -0500

    Update to work in atributos

---
+++ ProgrammingUnity/Assets/Scripts/03Atributos/Tooltip_HelpUrl.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProVideoGames.Atributos
+{
+    [HelpURL("https://github.com/Juan8516")]
+    public class Tooltip_HelpUrl : MonoBehaviour
+    {
+        [Tooltip("Link access repositories")]
+        public string git = "Access to Git";
+    }
+}
