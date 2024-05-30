ff3567633a13d9872dad61148105e59c6bb94162
Juan Manuel Salazar <134428143+Juan8516@users.noreply.github.com>
2024-05-30 14:02:08 -0500

    Update ForForeachWhile.cs

--- ProgrammingUnity/Assets/Scripts/04ControlStruct/ForForeachWhile.cs
+++ ProgrammingUnity/Assets/Scripts/04ControlStruct/ForForeachWhile.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace ProVideoGames.ControlStruct
 {
     public class ForForeachWhile : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public int[] waponAmmoArray;
+
+        private void CheckWeaponAmmo()
         {
-        
+            for(int i = 0; i  < waponAmmoArray.Length; i++)
+            {
+                Debug.Log($"Index {i} - Ammo: {waponAmmoArray[i]}");
+            }
+
+            foreach(int ammo in waponAmmoArray)
+            {
+                Debug.Log($"Index ? - Ammo: {ammo}");
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private IEnumerator Fade()
         {
-        
+            float alpha = 1;
+
+            while(true)
+            {
+                alpha -= Time.deltaTime;
+                yield return null;
+            }
+
+            //Do something
+
         }
     }
 }
