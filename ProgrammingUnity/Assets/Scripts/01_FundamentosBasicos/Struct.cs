38befff295fc92eb783915d4391a2d582ea89e66
Juan8516 <juan.ind85@gmail.com>
2024-09-06 11:44:17 -0500

    feat: Added struct EnemyData to manage enemy information.
    - Created EnemyData struct to store enemy name, health and status information.
    - Added variables to manage enemy name, health and life status in the struct.
    - Initialized the enemy data in the Start() method, including name, health and whether it is alive or dead.
    - Tested the correct initialization and use of EnemyData in the script logic.

--- ProgrammingUnity/Assets/Scripts/01_FundamentosBasicos/Struct.cs
+++ ProgrammingUnity/Assets/Scripts/01_FundamentosBasicos/Struct.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 namespace ProVideoGames.FundamentosProgramacion
 {
-        //Publico usable en otras clases
-        //[System.Serializable]
-        //public struct PlayerData
-        //{
-        //    public string name;
-        //    public int health;
-        //    public bool isDead;
-        //}
+    //Publico usable en otras clases
+    [System.Serializable]
+    public struct PlayerData
+    {
+        public string name;
+        public int health;
+        public bool isDead;
+    }
     public class Struct : MonoBehaviour
     {
-        //Conjunto de variables
+        //Conjunto variables player
         public string playerName;
         public int playerHealth;
         public bool playerIsDead;
@@ -23,19 +23,35 @@ namespace ProVideoGames.FundamentosProgramacion
         [System.Serializable]
         public struct PlayerData
         {
-            public string name;
-            public int health;
-            public bool isDead;
+            public string namePlayer;
+            public int healthPlayer;
+            public bool isDeadPLayer;
         }
 
         public PlayerData playerData;
 
-        private void Start()
+        //Conjunto variables enemigo.
+        public string nameEnemy;
+        public int healthEnemy;
+        public bool isDeadEnemy;
+
+        public struct EnemyData 
         {
-            playerData.name = "Juan Manuel Salazar";
-            playerData.health = 600;
-            playerIsDead = false;
+            public string nameEnemy;
+            public int healthEnemy;
+            public bool isDeadEnemy;
         }
 
+        public EnemyData enemyData;
+        private void Start()
+        {
+            playerData.namePlayer = "Juan Manuel Salazar";
+            playerData.healthPlayer = 600;
+            playerData.isDeadPLayer = false;
+
+            enemyData.nameEnemy = "Thanos";
+            enemyData.healthEnemy = 1000;
+            enemyData.isDeadEnemy = false;
+        }
     }
 }
