b1123696b57f4cbf785d063f8360f69493f05934
Juan8516 <juan.ind85@gmail.com>
2024-09-07 22:15:40 -0500

    Added heroesNames array in Array class
    Defined string array 'namesHeroes' to store hero names
    Initialized 'namesHeroesId' with values: Superman, Batman, and Flash
    Updated Start method to access elements from 'enemyIds' and 'namesHeroesId'

--- ProgrammingUnity/Assets/Scripts/01_FundamentosBasicos/Array.cs
+++ ProgrammingUnity/Assets/Scripts/01_FundamentosBasicos/Array.cs
@@ -8,14 +8,21 @@ namespace ProVideoGames.FundamentosProgramacion
     public class Array : MonoBehaviour
     {
         public int[] enemyId;
+        public string[] namesHeroes;
 
         //Forma poco usada en unity
         public int[] enemyIds = {100, 24, 543, 900};
+
+        public string[] namesHeroesId = { "Superman", "Batman", "Flash" };
 
         private void Start()
         {
             int myId = enemyIds[3]; //900
                 myId = enemyIds[0]; //100
+
+            string myHero = namesHeroesId[0];
+                   myHero = namesHeroesId[2];
         }
+
     }
 }
